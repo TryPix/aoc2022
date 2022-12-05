6675643fc076f66fe3f6d2e1c63bf39f3c3e3997
TryPix <115654449+TryPix@users.noreply.github.com>
2022-12-05 19:21:17 +0100

    Add files via upload

---
+++ AocTemplate.java
@@ -0,0 +1,35 @@
+package aoc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class AocTemplate {
+
+	public static void main(String[] args) throws FileNotFoundException  {
+		// TODO Auto-generated method stub
+		
+		System.out.println(foo(new File("aoc.txt")));
+
+	}
+	
+	public static int foo(File input) throws FileNotFoundException {
+		
+		
+		Scanner scan = new Scanner(input);
+		
+		while(scan.hasNextLine()) {
+			String line = scan.nextLine();
+			Scanner lineScan = new Scanner(line);
+			
+
+			lineScan.close();
+		}
+		
+		scan.close();
+		
+		return -1;
+		
+	}
+
+}
