55ab4b673139ac22e8231d70b6626e55601c6212
Saimaneesh <yeturu.saimaneesh@gmail.com>
2022-12-08 19:26:21 +0100

    improvement of the template

--- AocTemplate.java
+++ AocTemplate.java
@@ -1,8 +1,7 @@
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class AocTemplate {
 	public static void main(String[] args) throws FileNotFoundException  {
