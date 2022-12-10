f968d5271d5d7e3c87e76675a503df002cd236fc
Saimaneesh <yeturu.saimaneesh@gmail.com>
2022-12-10 13:33:17 +0100

    day10

--- day10/CathodeRayTube.java
+++ day10/CathodeRayTube.java
@@ -1,38 +1,86 @@
+package day10;
 
 import java.io.*;
 import java.util.*;
 
-public class AOC10 {
+public class CathodeRayTube {
+	
 	public static void main(String[] args) throws FileNotFoundException  {
 		
-		File in = new File("day/input.txt");
+		File in = new File("day10/input.txt");
 		
-		System.out.println(foo(in, false));
-		System.out.println(foo(in, true));
-
+		System.out.println("Part 1: " +  cycleSum(in));
+		System.out.println("Part 2: ");
+		crt(parse(in));
+		
 	}
 	
-	public static int foo(File input, boolean part2) throws FileNotFoundException {
+	
+	public static Integer cycleSum(File input) throws FileNotFoundException {
 		
+		Map<Integer, Integer> registerX = parse(input);
+		int sum = 0;
+		for (int i = 20; i < 221; i+= 40) {
+			sum += registerX.get(i) * i;
+		}
+		return sum;
+	}
+	
+	
+	public static void crt(Map<Integer, Integer> registerX) {
+		int mod = 1;
+		String curr;
 		
-		Scanner scan = new Scanner(input);
-		
-		while(scan.hasNextLine()) {
-			String line = scan.nextLine();
-			Scanner lineScan = new Scanner(line);
-			
-
-			lineScan.close();
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 40; j++) {
+				if (j < 40 && j > 0 && (j == registerX.get(j + mod) ||j == registerX.get(j + mod) + 1 || j == registerX.get(j + mod)-1)) {
+					curr = "█";
+				} else if (j == 0 && (j == registerX.get(j + mod) ||j == registerX.get(j + mod) + 1)){
+					curr = "█";	
+				} else if (j == 39 && (j == registerX.get(j + mod) ||j == registerX.get(j + mod) - 1)) {
+					curr = "█";	
+				}
+				else {
+					curr = " ";
+				}
+				System.out.print(curr);
+			}
+			System.out.println();
+			mod += 40;
 		}
 		
+	}
+	
+	
+	public static Map<Integer, Integer> parse(File input) throws FileNotFoundException{
+		Scanner scan = new Scanner(input);
+		int cycles = 1;
+		int x = 1;
+		Map<Integer, Integer> registerX = new HashMap<>();
+		while(scan.hasNext()) {
+			String inst  = scan.next();
+	
+			switch (inst) {
+			case "noop":
+				registerX.put(cycles, x);
+				cycles++;
+				registerX.put(cycles, x);
+
+				break;
+			case "addx":
+				int n = scan.nextInt();
+				registerX.put(cycles, x);
+				cycles++;
+				registerX.put(cycles, x);
+				cycles++;
+				x += n;
+				registerX.put(cycles, x);
+			}
+		}
 		scan.close();
 		
-		if (part2) {
-			return -1;
-		}
-		
-		return -1;
-		
+		registerX.put(0, 1);
+		return registerX;
 	}
 
 }
